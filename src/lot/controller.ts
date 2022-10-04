472b7a7f46fe963fb6d9a993eb3e5b2a55529c12
jarrilla <jarrilla.services@gmail.com>
2022-10-04 12:48:17 -0400

    remove junk code

--- src/lot/controller.ts
+++ src/lot/controller.ts
@@ -1,9 +1,3 @@
 import { Router } from "express";
-import { LotModel } from './model';
 
 export const LotRouter = Router();
-
-LotRouter.get('/', async (_req, res) => {
-  const lots = await LotModel.find();
-  res.send({ lots });
-});
