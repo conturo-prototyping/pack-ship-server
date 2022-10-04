39311129897b8c878ced17300b0a0d8969c2ad7a
Mitch McCracken <mitchell@conturoprototyping.com>
2022-10-04 16:51:47 -0400

    Added logic including deleteRouteStep function.

---
+++ src/routeStep/controller.ts
@@ -0,0 +1,33 @@
+import express = require('express');
+import { ExpressHandler, HTTPError } from '../utils';
+const router = express.Router();
+import { RouteStepModel } from './model';
+
+
+module.exports = router;
+
+//routes
+router.delete('/', deleteRouteStep);
+
+
+
+//functions
+function deleteRouteStep(
+  req: { body: { routeStepId: any; }; }, 
+  res: any
+  ) {
+  ExpressHandler(
+    async () => {
+      const { routeStepId } = req.body;
+      if ( !routeStepId ) return HTTPError('no route step id provided');
+
+      const deletedRouteStep = await RouteStepModel.findByIdAndDelete( routeStepId );
+      if ( !deletedRouteStep ) return HTTPError('no route step found');
+
+      const data = { deletedRouteStep };
+      return { data };
+    },
+    res,
+    'deleting route step'
+  );
+}
