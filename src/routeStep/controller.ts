dad0edebecd89baf80eea5863abe965102c8f6e5
Mitch McCracken <mitchell@conturoprototyping.com>
2022-10-04 15:59:53 -0400

    Added content to controller (mainly putRouteStep function).

---
+++ src/routeStep/controller.ts
@@ -0,0 +1,38 @@
+import express = require('express');
+import { ExpressHandler, HTTPError } from '../utils';
+const router = express.Router();
+import { RouteStepModel } from './model';
+
+
+module.exports = router;
+
+//routes
+router.put('/', putRouteStep);
+
+
+
+//functions
+async function putRouteStep(
+  req: { body: { category: string; name: string; }; }, 
+  res: any
+  ) {
+  ExpressHandler(
+    async () => {
+      const { category, name } = req.body;
+
+      if ( !category ) return HTTPError('no step category');
+      if ( !name ) return HTTPError('no step name');
+
+      const newRouteStep = new RouteStepModel( { category, name });
+      await newRouteStep.save();
+      
+      const data = 'some words';
+      return { data };
+    },
+    res,
+    'putting route step'
+  );
+}
+
+
+
