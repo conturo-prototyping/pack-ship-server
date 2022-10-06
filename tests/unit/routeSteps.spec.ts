3fc0e030ba888f58a9b3b194ac12754a2e2f2c20
Mitch McCracken <mitchell@conturoprototyping.com>
2022-10-06 14:41:23 -0400

    Updated to set globals in all caps, removed helper function, attempted to update db string but having issues when using a remote db (will fix later).

--- tests/unit/routeSteps.spec.ts
+++ tests/unit/routeSteps.spec.ts
@@ -4,39 +4,38 @@ import { ChaiRequest } from "../config";
 import { MongoClient } from 'mongodb';
 require('../config'); // recommended way of loading root hooks
 
-const url = '/routeSteps';
+const URL = '/routeSteps';
 
-// set up dbUrl
-let dbUrl = process.env.MONGO_DB_URI;
-dbUrl = dbUrl!.substring(0, dbUrl!.lastIndexOf('--TEST') )
+// set up DB_URL
+let DB_URL = process.env.MONGO_DB_URI;
+console.debug(DB_URL)
+DB_URL = DB_URL!.substring(0, DB_URL!.lastIndexOf('--TEST') )
+// console.debug(DB_URL)
+const _DB_URL = DB_URL?.split('?');
+console.debug(_DB_URL);
+DB_URL = _DB_URL[0] + '-test?' + _DB_URL[1];
+console.debug(DB_URL)
 
-const client = new MongoClient( dbUrl, { useUnifiedTopology: true } );
+//FYI for this there is a permission issue of trying to go to a new db, currently I don't have a local db set up
 
-async function clientConnect() {
-  try {
-    await client.connect();
-    // console.debug('--- client connected ---');
-  } 
-  catch (error) {
-    console.debug(error);
-  }
-}
+const CLIENT = new MongoClient( DB_URL, { useUnifiedTopology: true } );
 
 describe('# ROUTE STEPS', () => {
   it('Should find 1 inserted routeStep from collection.' , async () => {
 
     // set up connection to db
-    await clientConnect();
+    // await CLIENTConnect();
+    await CLIENT.connect();
 
     // create routeStep using mongodb driver
     const doc = {
       name: 'for testing',
       category: 'testing...'
     };
-    await client.db().collection('routeSteps').insertOne(doc);
+    await CLIENT.db().collection('routeSteps').insertOne(doc);
 
     // hit endpoint to get all routeSteps in collection
-    const res = await ChaiRequest('get', url + '/');
+    const res = await ChaiRequest('get', URL + '/');
     const numOfRouteSteps = res.body.routeSteps.length;
 
     expect(numOfRouteSteps).to.be.eq(1);
