1a3e89c1e1c2ae191f4764e9b2ca8d7d4d490a06
jarrilla <jarrilla.services@gmail.com>
2022-10-13 16:25:24 -0400

    remove unnecessary calls

--- tests/unit/routeSteps.spec.ts
+++ tests/unit/routeSteps.spec.ts
@@ -8,11 +8,11 @@ require('../config'); // recommended way of loading root hooks
 const URL = '/routeSteps';
 
 // set up DB_URL - NOTE: this only works with a local db, if not set up use the /debug/reset route to generate data (this might not be needed)
-const DB_URL: string = process.env.MONGO_DB_URI || '';
+const DB_URL: string = process.env.MONGO_DB_URI!;
 
 // FYI for this there is a permission issue of trying to go to a new db, currently I don't have a local db set up
 
-const CLIENT = new MongoClient(DB_URL, { useUnifiedTopology: true });
+const CLIENT = new MongoClient(DB_URL);
 
 describe('# ROUTE STEPS', () => {
   it('Should find 1 inserted routeStep from collection.', async () => {
