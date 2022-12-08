0fafe12965ff0852bd7d30c797b18b8ba365f03e
Jose Arrillaga <jose@conturoprototyping.com>
2022-12-07 22:14:57 -0500

    Add file description

--- src/consumablePO/model.js
+++ src/consumablePO/model.js
@@ -1,3 +1,9 @@
+/* consumablePO
+ * Model for Consumable POs that will be generated via the "Purchasing Module"
+ *
+ * Note: the purchasing module is part of ShopQ-v2 and will not be entirely visible from here.
+ */
+
 const { Schema, model } = require("mongoose");
 const { ObjectId } = Schema.Types;
 
