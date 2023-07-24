c74492da6c01661894d7ee31ceca6abbb591a382
Jose Arrillaga <jose@conturoprototyping.com>
2023-07-23 20:32:14 -0400

    Update app.js
    Use existing env variable

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.CORS_CLIENT_URL, process.env.CORS_WORKFLOW_URL],
+    origin: [process.env.CORS_CLIENT_URL, process.env.SHOP_URL],
     credentials: true,
   })
 );
