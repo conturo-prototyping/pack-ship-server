5076cfb8eeb06dad3c7401b7c7901363f1164617
Jose Arrillaga <jose@conturoprototyping.com>
2023-07-23 20:34:45 -0400

    Update app.js
    typo

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.CORS_CLIENT_URL, process.env.SHOP_URL],
+    origin: [process.env.CORS_CLIENT_URL, process.env.SHOPQ_URL],
     credentials: true,
   })
 );
